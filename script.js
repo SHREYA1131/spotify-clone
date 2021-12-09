5ac93985cd1044ddaefb4288bb955b22350ccce6
SHREYA1131 <shreyagupta4049@gmail.com>
2021-12-09 23:27:46 +0530

    cover images/names changed

--- script.js
+++ script.js
@@ -2,24 +2,31 @@ console.log("Welcome to spotify");
 
 //initialise
 let songindex = 0;
-let audioElement = new Audio('1tereliye.mp3');
+let audioElement = new Audio('songs/1tereliye.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myprogressbar');
 let gif= document.getElementById('gif');
+let songitems = Array.from(document.getElementsByClassName('songitem'));
 
 let songs = [
-    {songname: "tere liye", filePath: "songs/1tereliye.mp3", coverPath: "cover/1.jpg"},
-    {songname: "tere liye", filePath: "songs/2maahiVe.mp3", coverPath: "cover/2.jpg"},
-    {songname: "tere liye", filePath: "songs/3Pakeezah.mp3", coverPath: "cover/3.jpg"},
-    {songname: "tere liye", filePath: "songs/4MereHaathMein.mp3", coverPath: "cover/4.jpg"},
-    {songname: "tere liye", filePath: "songs/5Sahiba.mp3", coverPath: "cover/5.jpg"},
-    {songname: "tere liye", filePath: "songs/6TumHo.mp3", coverPath: "cover/6.jpg"},
-    {songname: "tere liye", filePath: "songs/7Latoo.mp3", coverPath: "cover/7.jpg"},
-    {songname: "tere liye", filePath: "songs/8KaiseMujhe.mp3", coverPath: "cover/8.jpg"},
-    {songname: "tere liye", filePath: "songs/9ChupChupKe.mp3", coverPath: "cover/9.jpg"},
-    {songname: "tere liye", filePath: "songs/10BaarishKaAsar.mp3", coverPath: "cover/10.jpg"},
+    {songname: "tere liye - Veer Zara", filePath: "songs/1tereliye.mp3", coverPath: "cover/1.jpg"},
+    {songname: "Maahi Ve - Highway", filePath: "songs/2maahiVe.mp3", coverPath: "cover/2.jpg"},
+    {songname: "Pakeezah - Ungli", filePath: "songs/3Pakeezah.mp3", coverPath: "cover/3.jpg"},
+    {songname: "Mere Haath Mein - Fanaa", filePath: "songs/4MereHaathMein.mp3", coverPath: "cover/4.jpg"},
+    {songname: "Sahiba - Phillauri", filePath: "songs/5Sahiba.mp3", coverPath: "cover/5.jpg"},
+    {songname: "Tum Ho - Rockstar", filePath: "songs/6TumHo.mp3", coverPath: "cover/6.jpg"},
+    {songname: "Latoo - Ghajini", filePath: "songs/7Latoo.mp3", coverPath: "cover/7.jpg"},
+    {songname: "Kaise Mujhe - Ghajini", filePath: "songs/8KaiseMujhe.mp3", coverPath: "cover/8.jpg"},
+    {songname: "Chup Chup Ke - Rush", filePath: "songs/9ChupChupKe.mp3", coverPath: "cover/9.jpg"},
+    {songname: "Baarish Ka Asar - Twin Strings", filePath: "songs/10BaarishKaAsar.mp3", coverPath: "cover/10.jpg"},
 ]
 //audioElement.play();
+
+songitems.forEach((element, i)=>{
+    console.log(element, i);
+    element.getElementsByClassName("coverimg")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songname")[0].innerText = songs[i].songname;
+})
 
 //play/pause
 masterPlay.addEventListener('click', ()=>{
